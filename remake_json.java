5b19700b7e79a2f8f6dfa311e4c1fa7deb780edc
Choyunhui <a01022371341@gmail.com>
2020-07-14 16:23:45 +0900

    잘못된 json파일 올바른 형식으로 고친후 덮어쓰기
    {  } {  } {  }  -> [  {  } , {  } , {  } ]   잘못된 json 파일을 불러올수 있도록, 올바르게 고친 후 덮어씀
    
    string 으로 json 파일의 내용들을 한 문장씩 불러들이고, 맨 앞과 맨 마지막에 각각   '['  와   ']'  를 추가, 각 문장 당 객체들 사이에  ',' 추가 해주기

---
+++ remake_json.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class remake_json {
+
+   public static void main(String[] args) throws IOException {
+
+      String[] a = new String[10000];
+      int i = 0;
+      File file = new File(
+            "C:\\Users\\a0102\\Desktop\\test.json");
+      if (file.exists()) {
+         BufferedReader inFile = new BufferedReader(new FileReader(file));
+         String sLine = null;
+         while ((sLine = inFile.readLine()) != null) {
+            a[i] = sLine;
+            i++;
+         }
+
+         a[0] = '[' + a[0];
+         a[i - 1] = a[i - 1] + ']';
+         for (int m = 0; m < i; m++) {
+            // System.out.println(a[j]);
+            if (a[m] != null && a[m + 1] != null && a[m].equals("}") && a[m + 1].equals("{")) {
+               a[m] = a[m] + ',';
+            }
+
+         }
+      }
+      try (FileWriter fout = new FileWriter("c:\\csvdata\\2020-01-01.json")) {
+         PrintWriter out = new PrintWriter(fout);
+         for (String line : a) {
+            if (line != null)
+               out.println(line);
+         }
+      }
+   }
+}
